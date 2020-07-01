152d719f6305e66f2ad82783892b9ff183ae9bc3
zhiwen yao <yzw@zhiwendeMBP.lan>
2020-07-01 17:28:25 +0800

    ..

--- frontend/views/common/comment.php
+++ frontend/views/common/comment.php
@@ -23,7 +23,7 @@ $urlManager = Yii::$app->urlManager;
             </div>
             <div class="content">
                 <strong>{{item.nickname}}</strong>
-                <div style="word-wrap: break-word;" class="text">
+                <div style="word-wrap: break-word;max-width: 100px" class="text">
                     {{item.content}}
                 </div>
                 <footer>{{item.addtime}}</footer>
