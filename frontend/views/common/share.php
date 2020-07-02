95aa664ceba5b62a0c1e418566289e6a66b93dc6
zhiwen yao <yzw@zhiwendeMBP.lan>
2020-07-02 09:15:04 +0800

    ..

--- frontend/views/common/share.php
+++ frontend/views/common/share.php
@@ -61,6 +61,11 @@ $urlManager = Yii::$app->urlManager;
         })
     });
 
+    wx.error(function(res){
+        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+        console.log(res)
+    });
+
     // 分享
     $('#shareClickEvent').on('click', '.ShareBox', function(e) {
         var chil = $(this).children();
