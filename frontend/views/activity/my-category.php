427c913083a854b19ca55c582b33c2d9a9a0f6f8
zhiwen yao <yzw@zhiwendeMBP.lan>
2020-07-02 15:12:35 +0800

    ..

--- frontend/views/activity/my-category.php
+++ frontend/views/activity/my-category.php
@@ -19,49 +19,56 @@ $this->title = '我的活动';
             <div class="card mb-3 mt-2">
 
                 <div class="card-header">
-                    活动时间：<?= $value['addtime'] . '----' . $value['endtime'] ?>
+                    活动时间：<?= $value['addtime'] . '~' . $value['endtime'] ?>
                 </div>
 
                 <div class="card-body">
                     <h5 class="card-title"><?= $value['name'] ?></h5>
                     <p class="card-text"><?= $value['theme'] ?></p>
                     <?php if (!$value['is_pay']): ?>
-                        <button type="button" class="btn btn-primary shadowed mr-1 mb-1">付费</button>
+                        <button type="button" id="goPayPage" class="btn btn-primary shadowed mr-1 mb-1">付费</button>
                     <?php endif; ?>
 
                     <?php if (!$value['is_join']): ?>
-                        <button type="button" class="btn btn-secondary shadowed mr-1 mb-1">签到</button>
+                        <button type="button" id="signIn" class="btn btn-secondary shadowed mr-1 mb-1">签到</button>
                     <?php endif; ?>
                 </div>
 
                 <div class="card-footer">
-
-                    <?php if ($value['is_pay']): ?>
-                        <span class="badge badge-success iconed mr-1 mb-1">
-                            <i class="icon ion-ios-checkmark"></i>
-                            已付费
-                            </span>
-                    <?php else: ?>
                         <span class="badge badge-primary iconed mr-1 mb-1">
-                            <i class="icon ion-ios-close"></i>
-                            未付费
+                            <?php if ($value['is_pay']): ?>
+                                    <i class="icon ion-ios-checkmark"></i>
+                                    已付费
+                            <?php else: ?>
+                                    <i class="icon ion-ios-close"></i>
+                                    未付费
+                            <?php endif; ?>
                         </span>
-                    <?php endif; ?>
 
-                    <?php if ($value['is_join']): ?>
                         <span class="badge badge-success iconed mr-1 mb-1">
-                            <i class="icon ion-ios-checkmark"></i>
-                            已签到
+                            <?php if ($value['is_join']): ?>
+                                    <i class="icon ion-ios-checkmark"></i>
+                                    已签到
+                            <?php else: ?>
+                                    <i class="icon ion-ios-close"></i>
+                                    未签到
+                            <?php endif; ?>
                         </span>
-                    <?php else: ?>
-                        <span class="badge badge-primary iconed mr-1 mb-1">
-                            <i class="icon ion-ios-close"></i>
-                            未签到
-                        </span>
-                    <?php endif; ?>
                 </div>
             </div>
         <?php endforeach ?>
     </div>
 
 </div>
+
+<script>
+    // 微信支付
+    $('#goPayPage').on('click',function (e) {
+
+    });
+
+    // 生成一个二维码
+    $('#signIn').on('click',function (e) {
+
+    });
+</script>
