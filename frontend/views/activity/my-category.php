a30dbc38ce60507a511d48a4ae9bf3838a29236a
zhiwen yao <yzw@zhiwendeMBP.lan>
2020-07-02 18:14:38 +0800

    ..

--- frontend/views/activity/my-category.php
+++ frontend/views/activity/my-category.php
@@ -19,8 +19,8 @@ $this->title = '我的活动';
             <div class="card mb-3 mt-2">
 
                 <div class="card-header">
-                    <p>活动时间：</p>
-                    <small><?= $value['addtime'] . '~' . $value['endtime'] ?></small>
+                    <small>活动时间：
+                        <?= $value['addtime'] . '~' . $value['endtime'] ?></small>
                 </div>
 
                 <div class="card-body">
