1f88018ad93b8177b514aca1ac0cff795cb8d275
zhiwen yao <yzw@zhiwendeMBP.lan>
2020-07-02 11:36:47 +0800

    ..

--- frontend/views/user-info/index.php
+++ frontend/views/user-info/index.php
@@ -193,6 +193,10 @@ $urlManager = Yii::$app->urlManager;
 </div>
 
 <script>
+    $.ready(function () {
+        var val = $("#birthday").attr('valye')
+        $("#birthday").val(val.split(' ').join("T"))
+    })
     // 获取参数
     $.ajax({
         url: "<?= $urlManager->createUrl(['user-info/index']) ?>",
