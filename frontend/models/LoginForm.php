aa9570fa2a296d72a2ef16c50b151e75249dd870
zhiwen yao <yzw@zhiwendeMBP.lan>
2020-07-21 17:27:04 +0800

    ..

--- frontend/models/LoginForm.php
+++ frontend/models/LoginForm.php
@@ -95,6 +95,7 @@ class LoginForm extends BaseModel
             }
             if ($res) {
                 $duration = \Yii::$app->user->authtimeout;
+                var_dump($this->getUser());exit;
                 \Yii::$app->user->login($this->getUser(), $duration);
             } else {
                 return $this->resultMsg(null, ConstStatus::CODE_ERROR,'登录失败请联系管理员');
